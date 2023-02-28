a54c76093d86dc437da1dd601a56618e3dd7e0c3
Jlupuka <dima.usachev.0790@mail.ru>
2023-02-28 18:17:04 +0500

    edits

--- Telegram_bot/bot_commands/commands.py
+++ Telegram_bot/bot_commands/commands.py
@@ -3,5 +3,5 @@ from aiogram import types
 
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
-        types.BotCommand('start', 'Начало работы бота'),
-        types.BotCommand('info', 'Выводит все возможные города')])
+        types.BotCommand('start', 'start work bot'),
+        types.BotCommand('info', 'displays all the cities that are in the bot')])
